9b2f81836c8fee6c3c04780fe757d86cc7e5cfae
ferryal <Ferryal21@gmail.com>
2018-01-16 11:00:34 +0700

    added materialize index.html

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-// NOTE: result get number
-function getNumber() {
-  var numbA = parseFloat(document.getElementById('input-one').value);
-  let numbB = 099;
+let buttonAdd = document.querySelector("#btn-tambah");
+let buttonSub = document.querySelector("#button-subs");
+let buttonMulti = document.querySelector("#button-multi");
+let buttonDiv = document.querySelector("#button-div");
+let buttonMod = document.querySelector('#button-mod');
+let buttonAddSub = document.querySelector('#button-addsubs');
 
-  document.getElementById('result').innerHTML = numbA;
-  console.log(numbB);
-}
 //NOTE: addition
-function add(numbA, numbB) {
+function addition() {
   var numbA = parseFloat(document.getElementById('input-one').value);
   var numbB = parseFloat(document.getElementById('input-two').value);
 
   document.getElementById('result').innerHTML = numbA + numbB;
 }
 // NOTE: substract
-function min(callback) {
+function min() {
   var numbA = parseFloat(document.getElementById('input-one').value);
   var numbB = parseFloat(document.getElementById('input-two').value);
 
@@ -44,42 +43,9 @@ function mod() {
 
   document.getElementById('result').innerHTML = numbA % numbB;
 }
-// NOTE: differents type (string & boolean) input type text / string
-function bol() {
-  var boolean = true;
-  var numbA = parseFloat(document.getElementById('input-one').value);
-  var numbB = parseFloat(document.getElementById('input-two').value);
-  if (boolean) {
-    var numbC = numbA / numbB;
-    var result = `${numbA} division ${numbB} equals ${numbC}`;
-    document.getElementById('result').innerHTML = result;
-  }
-}
-// NOTE:  try store kind of numbers into an array variable
-function arrType() {
-  var numbA = ['20.9', '100', '-2', '1'];
-  numbA.push('10');
-  numbA.push('-100')
-  console.log(numbA);
-  numbA.pop('10');
-  numbA.pop('-100');
-  numbA.pop('1');
-  console.log(numbA);
 
-  for (var i = 0; i < numbA.length; i++) {
-    console.log(numbA[i]);
-    document.getElementById('result').innerHTML = numbA;
-  }
-
-
-}
-
-document.getElementById('result-one').addEventListener('click', getNumber);
-document.getElementById('result-two').addEventListener('click', add);
-document.getElementById('result-three').addEventListener('click', min);
-document.getElementById('result-four').addEventListener('click', multiplication);
-document.getElementById('result-five').addEventListener('click', division);
-document.getElementById('result-six').addEventListener('click', mod);
-document.getElementById('result-seven').addEventListener('click', bol);
-document.getElementById('result-eight').addEventListener('click', arrType);
-// document.getElementById('result-nine').addEventListener('click', min(add));
+buttonAdd.addEventListener('click', addition);
+buttonSub.addEventListener('click', min);
+buttonMulti.addEventListener('click', multiplication);
+buttonDiv.addEventListener('click', division);
+buttonMod.addEventListener('click', mod);
